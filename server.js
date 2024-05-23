2dbef163e852147f71dfbd3d63b9d33fab1199e5
Saifaldin Elabyad <elabyad247@gmail.com>
2024-05-24 00:03:32 +0300

    mmitsadasdas

--- server.js
+++ server.js
@@ -14,9 +14,8 @@ const db = mysql.createConnection({
   database: process.env.DB_NAME,
 });
 
-app.listen(3001, () => {
-  console.log("Server is running on port 3001");
-});
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
